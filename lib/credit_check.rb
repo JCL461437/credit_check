b1bce6813440b252b83a544d4fd9bbe06228607a
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-21 14:08:35 -0600

    Adds the start of the class CreditCheck

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,3 +1,53 @@
+
+class CreditCheck 
+  attr_reader :card_number, :check_limit 
+
+  def initialize (card_number, limit)
+    # Your Luhn Algorithm Here
+    def is_valid?(card_number)
+      @card_number = card_number.to_i
+      @sum = 0
+      @alternate = false
+  
+        while @card_number > 0
+          digit = @card_number % 10
+          @card_number /= 10
+  
+          if alternate
+            digit *= 2
+            digit -= 9 if digit > 9
+          end
+  
+          sum += digit
+          alternate = !alternate
+        end
+  
+        if sum % 10 == 0
+          puts "The credit card number [#{@card_number}] is valid!"
+        else
+          puts "The credit card number [#{@card_number}] is invalid!"
+        end
+    
+  
+      puts @card_number
+      puts is_valid?(@card_number)
+    end 
+    
+    def card_number 
+    
+    end
+
+    def limit
+
+    end
+
+    def last_four
+      return last_four = @card_number[-4..-1]
+    end
+  end
+
+end
+
 card_number = "5541808923795240"
 # card_number = "4024007136512380"
 # card_number = "6011797668867828"
@@ -5,34 +55,11 @@ card_number = "5541808923795240"
 # card_number = "4024007106512380" 
 # card_number = "6011797668868728"
 
-# Your Luhn Algorithm Here
-def luhn_algo?(card_number)
-    number = card_number.to_i
-    sum = 0
-    alternate = false
-  
-    while number > 0
-      digit = number % 10
-      number /= 10
-  
-      if alternate
-        digit *= 2
-        digit -= 9 if digit > 9
-      end
-  
-      sum += digit
-      alternate = !alternate
-    end
-  
-    if sum % 10 == 0
-      puts "The credit card number [#{card_number}] is valid!"
-    else
-      puts "The credit card number [#{card_number}] is invalid!"
-    end
-  end
-  
-puts card_number
-puts luhn_algo?(card_number)
+limit = true
+
+credit_check = CreditCheck.new(card_number, limit)
+
+credit_check.last_four
 
 # Output
 # If it is valid, print ""The number [card number] is valid!"
